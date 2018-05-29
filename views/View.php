9fc5d5326c5d16b2d269afcbad4f27db6cf9fb30
KOCTET <ryabushenko2009@gmail.com>
2018-05-29 15:24:14 +0300

    подготовка

--- views/View.php
+++ views/View.php
@@ -1,5 +1,20 @@
 <?php
 
 class View {
-    
+      
+    /**
+     * Имя файла основного шаблона
+     * @var string 
+     */
+    public $template='main.php';
+    /**
+     * Имя файла отображения контента
+     * @var string
+     */
+    public $content_view;
+
+
+    public function show() {
+	include_once 'template/'. $this->template;
+    }
 }
